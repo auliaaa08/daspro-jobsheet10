86f5a701edb139b11d6eb6d7a0f4dcd9251002ff
auliaaa08 <ab.auliaabilqis@gmail.com>
2025-11-13 08:45:05 +0700

    Modifikasi 2

--- Bioskop06.java
+++ Bioskop06.java
@@ -1,5 +1,6 @@
 public class Bioskop06 {
     public static void main(String[] args) {
+
         String[][] penonton = new String[4][2];
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
@@ -14,5 +15,11 @@ public class Bioskop06 {
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+
+        System.out.println(penonton.length);
+        System.out.println(penonton[0].length);
+        System.out.println(penonton[1].length);
+        System.out.println(penonton[2].length);
+        System.out.println(penonton[3].length);
     }
 }
