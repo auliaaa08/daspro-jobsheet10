5d490faa5263fdeb09142417005588695edb9c83
auliaaa08 <ab.auliaabilqis@gmail.com>
2025-11-13 11:10:25 +0700

    Modifikasi 2

--- Numbers06.java
+++ Numbers06.java
@@ -7,7 +7,7 @@ public class Numbers06 {
         myNumbers[2] = new int[1];
 
         for (int i = 0; i < myNumbers.length; i++) {
-            System.out.print(Arrays.toString(myNumbers[i]));
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + myNumbers[i].length);
         }
     }
 }
